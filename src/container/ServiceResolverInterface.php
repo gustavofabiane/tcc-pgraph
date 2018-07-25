0438c6cc390044a4de581d12f25821f499ab5a28
Gustavo Fabiane <gustavfabiane@gmail.com>
2018-07-25 00:21:06 -0300

    :memo: complements docblocks in ServiceResolverInterface

--- src/container/ServiceResolverInterface.php
+++ src/container/ServiceResolverInterface.php
@@ -10,12 +10,18 @@ use Psr\Container\ContainerInterface;
 interface ServiceResolverInterface
 {
     /**
-     * RegEx pattern for string resolvables
+     * RegEx pattern for string class:method resolvables
      */
     const RESOLVABLE_PATTERN = '!^([^\:]+)\:([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)$!';
     
     /**
-     * Resolves a class, method or Closure
+     * Resolves a class, method or Closure|callable
+     * 
+     * MUST accept the following structures:
+     * -> a class name
+     * -> a string with a class name and a method name separated by ':', ex: class:method
+     * -> a \Closure instance
+     * -> a callable
      *
      * @param mixed $resolvable
      * @param bool $deffered define that the resolver instance will return a Closure for deffered resolving
@@ -23,7 +29,18 @@ interface ServiceResolverInterface
      */
     public function resolve($resolvable, bool $deffered = false, array $parameters = []);
 
+    /**
+     * Defines a ContainerInterface instance to be used by the resolver
+     *
+     * @param ContainerInterface $container
+     * @return void
+     */
     public function setContainer(ContainerInterface $container);
 
+    /**
+     * Retrieves the ContainerInterface instance used by the resolver
+     *
+     * @return ContainerInterface
+     */
     public function getContainer(): ContainerInterface;
 }
