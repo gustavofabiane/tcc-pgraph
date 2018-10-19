0a4b588a8ea39f02c2ffbf91b33cd1219e984da9
Gustavo Fabiane <gustavfabiane@gmail.com>
2018-10-19 00:08:27 -0300

    :pencil2: fix typo on container for resolvable middleware instance

--- src/Http/Handlers/HasMiddlewareTrait.php
+++ src/Http/Handlers/HasMiddlewareTrait.php
@@ -114,7 +114,7 @@ trait HasMiddlewareTrait
         if (is_callable($middleware) || $middleware instanceof \Closure || 
            is_string($middleware) && (preg_match(ServiceResolverInterface::RESOLVABLE_PATTERN, $middleware, $matches))
         ) {
-            return new ResolvableMiddleware($middleware, $this);
+            return new ResolvableMiddleware($middleware, $this->container);
         }
 
         if (is_object($middleware) || is_string($middleware) && 
