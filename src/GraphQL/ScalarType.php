c80c7d256838d704c3baa732ff1666c604a713ea
Gustavo Fabiane <gustavfabiane@gmail.com>
2018-11-08 00:55:36 -0200

    :sparkles: (NO-TEST) abstract scalar type definition with strict typing

---
+++ src/GraphQL/ScalarType.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\GraphQL;
+
+use GraphQL\Language\AST\Node;
+use GraphQL\Type\Definition\ScalarType as BaseScalarType;
+
+/**
+ * Abstract implementation of a scalar type definitions.
+ */
+abstract class ScalarType extends BaseScalarType
+{
+    
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param string $value
+     * @return string
+     */
+    abstract public function serialize(string $value): string;
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input.
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    abstract public function parseValue($value);
+
+    /**
+     * Parses an externally provided literal value (hardcoded in GraphQL query) to use as an input.
+     * 
+     * E.g. 
+     * {
+     *   user(email: "user@example.com") 
+     * }
+     *
+     * @param Node $valueNode
+     * @return string
+     * @throws \GraphQL\Error\Error
+     */
+    abstract public function parseLiteral(Node $valueNode): string;
+}
